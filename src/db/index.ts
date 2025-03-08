0461800c8c5b93ebcaacd6bc4a9563e412d4270e
Neeraj Dalal <admin@nrjdalal.com>
2025-03-08 12:25:09 +0530

    feat: better-auth 🔥 bun.lock, drizzle.config.ts, package.json, src/a... 🔥 10 files changed 271+ 3-
    bun.lock
    drizzle.config.ts
    package.json
    src/app/api/auth/[...all]/route.ts
    src/db/index.ts
    src/db/schema/auth.ts
    src/db/schema/index.ts
    src/lib/auth/client.ts
    src/lib/auth/index.ts
    src/middleware.ts

---
+++ src/db/index.ts
@@ -0,0 +1,35 @@
+import { authSchema } from "@/db/schema"
+import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+
+declare global {
+  // eslint-disable-next-line
+  var db: PostgresJsDatabase
+}
+
+let db: PostgresJsDatabase
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle({
+    client: postgres(process.env.POSTGRES_URL!, {
+      connect_timeout: 10000,
+      idle_timeout: 30000,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    }),
+  })
+} else {
+  if (!global.db) {
+    global.db = drizzle({
+      client: postgres(process.env.POSTGRES_URL!, {
+        connect_timeout: 10000,
+        idle_timeout: 30000,
+        max: 20,
+      }),
+    })
+  }
+  db = global.db
+}
+
+export { authSchema, db }
---
+++ src/db/schema/index.ts
@@ -0,0 +1,3 @@
+import { account, session, user, verification } from "@/db/schema/auth"
+
+export const authSchema = { account, session, user, verification }
